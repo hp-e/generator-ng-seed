7909ec672d66a72780f146d595b09972d8fd6aec
Hans-Petter Eitvet <hans-petter.eitvet@avento.no>
2016-12-09 14:51:41 +0100

    code cleaning

--- generators/add/templates/ng2/_ng2.Pages.ts
+++
@@ -1,7 +0,0 @@
-// this file can be used to export your components in one file
-
-// export * from './pages/your.component';
-// ...
-
-export * from './pages/<%= singularKebabName %>.index.page';
-export * from './pages/<%= singularKebabName %>.edit.page';
