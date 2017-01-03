64904b983b7e9722f401634355f82595ca48eb52
Hans-Petter Eitvet <hans-petter.eitvet@avento.no>
2017-01-03 14:40:49 +0100

    added lifecycles hook to component

--- generators/component/templates/ng2/_ng2.component.ts
+++ generators/component/templates/ng2/_ng2.component.ts
@@ -1,11 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, OnDestroy, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: '<%= pageName %>',
   styleUrls: ['./<%= pageName %>.component.css'],
   templateUrl: './<%= pageName %>.component.html'
 })
-export class <%= className %>Component {
+export class <%= className %>Component implements OnInit, OnDestroy, OnChanges {
 
   constructor() { }
+  ngOnInit() { }
+  ngOnDestroy() { }
+  ngOnChanges(changes: SimpleChanges) {}
 }
